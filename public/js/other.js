45e2236bb95465b39f944abf952b7d259f4d8a47
srikanth <sree2chin@gmail.com>
2017-08-03 00:04:18 +0530

    change transition of images

--- public/js/other.js
+++ public/js/other.js
@@ -18,8 +18,14 @@ var imagesList = [
 	"http://i68.tinypic.com/2lt4x05.jpg",
 	"http://i68.tinypic.com/4ubd5y.jpg",
 	"http://i65.tinypic.com/10e1sfq.jpg",
-	"http://i64.tinypic.com/2010pw9.jpg"
+	"http://i64.tinypic.com/2010pw9.jpg",
+	"http://i66.tinypic.com/9qw9sm.jpg",
+	"http://i64.tinypic.com/35lgnqa.jpg"
 ]
+
+var currentImage = "s-dynamic-image";
+
+$('.s-dynamic-image2').addClass("s-clickable-image-dum");
 
 $( document ).ready(function() {
 	setTimeout(function(){
@@ -32,16 +38,36 @@ $( document ).ready(function() {
 			img.appendTo('.s-grid-inner-container');
 	    }
 	    $(document).on("click",".s-clickable-image",function() {
+	    	var tempCurrentImage = currentImage;
+	    	var tempCurrentImage2 = currentImage == "s-dynamic-image" ? "s-dynamic-image2" : "s-dynamic-image";
 	    	var newSrc = $(this)[0].src;
-	    	if($('.s-dynamic-image').attr("src") != newSrc) {
+	    	if($('.'+tempCurrentImage).attr("src") != newSrc) {
 	    		$('#s-dynamic-img-href').attr('href', newSrc);
-		    	$('.s-dynamic-image').removeClass("s-clickable-image-open");
-		    	$('.s-dynamic-image').addClass("s-clickable-image-close");
+		    	
+		    	//remove the current div classes
+		    	$('.'+tempCurrentImage).removeClass("s-clickable-image-open");
+		    	$('.'+tempCurrentImage).addClass("s-clickable-image-close");
 		    	setTimeout(function(){
-		    		$('.s-dynamic-image').attr("src", newSrc);
-		    		$('.s-dynamic-image').removeClass("s-clickable-image-close");
-		    		$('.s-dynamic-image').addClass("s-clickable-image-open");
+		    		$('.'+tempCurrentImage).removeClass("s-clickable-image-close");
+		    		$('.'+tempCurrentImage).addClass("s-clickable-image-dum");
 		    	},500)
+		    	// add new classes to the new div
+		    	setTimeout(function(){
+		    		$('.'+tempCurrentImage2).attr("src", newSrc);
+		    		$('.'+tempCurrentImage2).addClass("s-clickable-image-dum");
+		    		$('.'+tempCurrentImage2).removeClass("s-clickable-image-dum");
+		    		// $('.'+tempCurrentImage2).removeClass("s-clickable-image-dum");
+		    		$('.'+tempCurrentImage2).addClass("s-clickable-image-open");
+
+		    		currentImage = tempCurrentImage2;
+		    	},250)
+		    	
+		    	
+		    	// setTimeout(function(){
+		    	// 	$('.s-dynamic-image').attr("src", newSrc);
+		    	// 	$('.s-dynamic-image').removeClass("s-clickable-image-close");
+		    	// 	$('.s-dynamic-image').addClass("s-clickable-image-open");
+		    	// },500)
 	    	}
 	    });
 	}, 100)  
